e0cf99fb1b2f8e2c9a17c5ab2c0b3954106d2319
Karel Ježík <jezikkarel@gmail.com>
2023-06-10 19:22:35 +0200

    vb

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: {
-    DB_CONNECT_STRING:
-      "mongodb+srv://user:7o5InmsZJg2FqIgE@cluster6.jjfy6nr.mongodb.net/cluster6",
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
