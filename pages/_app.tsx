f5f1fc2f7baf37ed02668a8f54f8222cfff421a0
Karel Ježík <jezikkarel@gmail.com>
2023-06-13 20:25:49 +0200

    vercelaalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,29 +4,16 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import Layout from "@/components/layout/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  //makes fetched data fresh for 60 seconds - no refetch for this perios
   const [queryClient] = useState(() => new QueryClient());
-
-  // const queryClient = new QueryClient({
-  //   // Disable automatic retries on failed requests
-  //   defaultOptions: {
-  //     queries: {
-  //       staleTime: 1000 * 60,
-  //       cacheTime: 1000 * 60,
-  //       refetchOnMount: false,
-  //       refetchOnWindowFocus: false,
-  //       retry: false,
-  //     },
-  //   },
-  // });
-
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
