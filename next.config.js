6c11a33ba342cea4ebc5957910e55ea24ea1058f
Karel Jezik <karel.jezik@styrax.cz>
2023-06-20 17:07:39 +0200

    5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /\*_ @type {import('next').NextConfig} _/;
 const nextConfig = {
   reactStrictMode: false,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
