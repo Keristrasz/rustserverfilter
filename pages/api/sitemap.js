86f200e062727777050ad1310ff65a760ab86e5d
Karel Jezik <karel.jezik@styrax.cz>
2023-07-17 14:58:22 +0200

    s

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     console.log(app);
   }
 
-  const initialData = await fetchAllServers(initialFilter, initialSorter, 0, 10, app);
+  const initialData = await fetchAllServers(initialFilter, initialSorter, 0, 500, app);
   console.log(initialData);
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
