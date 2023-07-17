47a9194909d131cb4bbc17065f792d9ea5c841fa
Karel Jezik <karel.jezik@styrax.cz>
2023-07-17 13:55:37 +0200

    sss

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,5 +1,5 @@
-import { writeFileSync } from 'fs';
-import { globby } from 'globby';
+const fs = require("fs");
+const globby = require("globby");
 
 function addPage(page) {
   const path = page.replace("pages", "").replace(".js", "").replace(".mdx", "");
@@ -18,7 +18,7 @@ async function generateSitemap() {
 ${pages.map(addPage).join("\n")}
 </urlset>`;
 
-  writeFileSync("public/sitemap.xml", sitemap);
+  fs.writeFileSync("public/sitemap.xml", sitemap);
 }
 
 generateSitemap();
