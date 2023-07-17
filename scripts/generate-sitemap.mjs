ac48abe813f943a32da0035369f797825184be78
Karel Jezik <karel.jezik@styrax.cz>
2023-07-17 13:25:48 +0200

    s

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,47 +1,24 @@
-import { writeFileSync } from "fs";
-import { globby } from "globby";
-import prettier from "prettier";
+const fs = require("fs");
+const globby = require("globby");
 
-async function generate() {
-  const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
-  const pages = await globby([
-    "pages/*.js",
-    "data/**/*.mdx",
-    "!data/*.mdx",
-    "!pages/_*.js",
-    "!pages/api",
-    "!pages/404.js",
-  ]);
+function addPage(page) {
+  const path = page.replace("pages", "").replace(".js", "").replace(".mdx", "");
+  const route = path === "/index" ? "" : path;
 
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${pages
-          .map((page) => {
-            const path = page
-              .replace("pages", "")
-              .replace("data", "")
-              .replace(".js", "")
-              .replace(".mdx", "");
-            const route = path === "/index" ? "" : path;
-
-            return `
-              <url>
-                  <loc>${`https://rustserverfilter${route}`}</loc>
-              </url>
-            `;
-          })
-          .join("")}
-    </urlset>
-    `;
-
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: "html",
-  });
-
-  // eslint-disable-next-line no-sync
-  writeFileSync("public/sitemap.xml", formatted);
+  return `  <url>
+    <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
+    <changefreq>hourly</changefreq>
+  </url>`;
 }
 
-generate();
+async function generateSitemap() {
+  // Ignore Next.js specific files (e.g., _app.js) and API routes.
+  const pages = await globby(["pages/**/*{.js,.mdx}", "!pages/_*.js", "!pages/api"]);
+  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(addPage).join("\n")}
+</urlset>`;
+
+  fs.writeFileSync("public/sitemap.xml", sitemap);
+}
+
+generateSitemap();
