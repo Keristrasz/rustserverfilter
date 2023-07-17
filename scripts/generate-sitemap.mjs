c10ab17f3b8798f75c09ae60d569d1f941d4e13c
Karel Jezik <karel.jezik@styrax.cz>
2023-07-17 13:38:54 +0200

    s2

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,24 +1,47 @@
-const fs = require("fs");
-const globby = require("globby");
+import { writeFileSync } from "fs";
+import { globby } from "globby";
+import prettier from "prettier";
 
-function addPage(page) {
-  const path = page.replace("pages", "").replace(".js", "").replace(".mdx", "");
-  const route = path === "/index" ? "" : path;
+async function generate() {
+  const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
+  const pages = await globby([
+    "pages/*.js",
+    "data/**/*.mdx",
+    "!data/*.mdx",
+    "!pages/_*.js",
+    "!pages/api",
+    "!pages/404.js",
+  ]);
 
-  return `  <url>
-    <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
-    <changefreq>hourly</changefreq>
-  </url>`;
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace("pages", "")
+              .replace("data", "")
+              .replace(".js", "")
+              .replace(".mdx", "");
+            const route = path === "/index" ? "" : path;
+
+            return `
+              <url>
+                  <loc>${`https://rustserverfilter.com${route}`}</loc>
+              </url>
+            `;
+          })
+          .join("")}
+    </urlset>
+    `;
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: "html",
+  });
+
+  // eslint-disable-next-line no-sync
+  writeFileSync("public/sitemap.xml", formatted);
 }
 
-async function generateSitemap() {
-  // Ignore Next.js specific files (e.g., _app.js) and API routes.
-  const pages = await globby(["pages/**/*{.js,.mdx}", "!pages/_*.js", "!pages/api"]);
-  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(addPage).join("\n")}
-</urlset>`;
-
-  fs.writeFileSync("public/sitemap.xml", sitemap);
-}
-
-generateSitemap();
+generate();
