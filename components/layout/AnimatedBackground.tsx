252925ff1e04aff14e187ff241bf029c24208344
Karel Jezik <karel.jezik@styrax.cz>
2023-07-24 13:58:23 +0200

    video clip fix

--- components/layout/AnimatedBackground.tsx
+++ components/layout/AnimatedBackground.tsx
@@ -4,7 +4,7 @@ const AnimatedBackground = () => {
       {/* <video className="w-full h-full object-cover " autoPlay loop muted>
         <source src="/sea1080_2.mp4" type="video/mp4" />
       </video> */}
-      <video className="w-full h-full object-cover" autoPlay loop muted>
+      <video className="w-full h-auto object-cover" autoPlay loop muted>
         <source src="/rust_dome.mp4" type="video/mp4" />
       </video>
     </div>
