dabc4cc29d09f0a4ca20060589b8dece87993368
Karel Jezik <karel.jezik@styrax.cz>
2023-07-24 14:25:39 +0200

    s

--- components/layout/AnimatedBackground.tsx
+++ components/layout/AnimatedBackground.tsx
@@ -1,6 +1,6 @@
 const AnimatedBackground = () => {
   return (
-    <div className="w-full h-full absolute z-[-1] overflow-hidden mt-20 max-h-screen">
+    <div className="w-full h-full absolute z-[-1] overflow-hidden">
       {/* <video className="w-full h-full object-cover " autoPlay loop muted>
         <source src="/sea1080_2.mp4" type="video/mp4" />
       </video> */}
