f31c8b0c3f7806e700ed05fe5e34fab0d2e54671
Karel Ježík <jezikkarel@gmail.com>
2023-07-27 18:57:09 +0200

    sitemap update

--- next.config.js
+++ next.config.js
@@ -81,14 +81,14 @@ const nextConfig = {
 
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.xml",
-        destination: "/api/sitemap",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/sitemap.xml",
+  //       destination: "/api/sitemap",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
