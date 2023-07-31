62598e7efe917dbd2c1d4e25230defbaefa56a5c
Karel Jezik <karel.jezik@styrax.cz>
2023-07-31 08:26:00 +0200

    l

--- components/layout/AnimatedBackground.tsx
+++ components/layout/AnimatedBackground.tsx
@@ -2,10 +2,10 @@ const AnimatedBackground = () => {
   return (
     <div className="w-full max-h-[100%] absolute z-[-1] overflow-hidden">
       {/* <video className="w-full max-h-[100%] object-cover" autoPlay loop muted>
-        <source src="/sea1080_2.mp4" type="video/mp4" />
+        <source src="/sea1080-2.mp4" type="video/mp4" />
       </video> */}
       <video className="w-full max-h-[100%] object-cover" autoPlay loop muted>
-        <source src="/rust_dome.mp4" type="video/mp4" />
+        <source src="/rust-dome.mp4" type="video/mp4" />
       </video>
     </div>
   );
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -4,11 +4,16 @@ import Footer from "./Footer";
 import AnimatedBackground from "./AnimatedBackground";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  let isMobile = false;
+
+  const MOBILE_WIDTH_THRESHOLD = 800;
+  isMobile = typeof window !== "undefined" && window.innerWidth < MOBILE_WIDTH_THRESHOLD;
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex-grow relative">
-        <AnimatedBackground />
+        {!isMobile && <AnimatedBackground />}
         {children}
       </div>
       <Footer />
