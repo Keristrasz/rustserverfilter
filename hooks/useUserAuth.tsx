c8836a9d1534d01ea7b0d656ac7deafc95d568ff
Karel Ježík <jezikkarel@gmail.com>
2023-08-01 20:27:53 +0200

    fontupd

--- hooks/useUserAuth.tsx
+++ hooks/useUserAuth.tsx
@@ -5,7 +5,6 @@ const useUserAuth = () => {
   const auth = async () => {
     const app = Realm.getApp(process.env.NEXT_PUBLIC_APP_ID || "");
     if (app && !app.currentUser) {
-      ("app && !app.currentUser");
       const anonymousUser = Realm.Credentials.anonymous();
       await app.logIn(anonymousUser);
     }
