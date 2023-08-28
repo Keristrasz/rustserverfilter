7df72468604b6b7b9f763fc7b9068f8d0c012e66
Karel Ježík <jezikkarel@gmail.com>
2023-08-28 14:41:28 +0200

    gh

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import AnimatedBackground from "./AnimatedBackground";
+import { useEffect } from "react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   // causes hydration errors
-  let isMobile = false;
-
+  // let isVideoBackgroundRendered = false;
   const MOBILE_WIDTH_THRESHOLD = 800;
-  isMobile = typeof window !== "undefined" && window.innerWidth < MOBILE_WIDTH_THRESHOLD;
+
+  const [isVideoBackgroundRendered, setIsVideoBackgroundRendered] = useState(false);
+
+  useEffect(() => {
+    setIsVideoBackgroundRendered(
+      typeof window !== "undefined" && window.innerWidth > MOBILE_WIDTH_THRESHOLD
+    );
+  }, []);
+  // isMobile = typeof window !== "undefined" && window.innerWidth < MOBILE_WIDTH_THRESHOLD;
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex-grow relative">
-        {!isMobile && <AnimatedBackground />}
+        {isVideoBackgroundRendered && <AnimatedBackground />}
         {children}
       </div>
       <Footer />
