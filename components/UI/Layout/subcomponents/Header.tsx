c697e1fe1661fd73b0c352641d046d80c3e44425
Karel Jezik <karel.jezik@styrax.cz>
2023-10-02 09:32:52 +0200

    j

--- components/UI/Layout/subcomponents/Header.tsx
+++ components/UI/Layout/subcomponents/Header.tsx
@@ -13,7 +13,7 @@ function Header() {
       <Link href="/" className="flex justify-between items-center hover:text-white">
         <img src="/rust-server-logo.jpg" width={100} height={100} alt="rust server logo" />
         <div>
-          <h2 className="font-rust text-2xl sm:text-4xl tracking-[0.065rem] [text-shadow:_3px_3px_0_rgb(0_0_0_/_40%)]">
+          <h2 className="font-rust text-2xl sm:text-4xl tracking-[0.065rem] [text-shadow:_3px_3px_3px_rgb(0_0_0_/_60%)]">
             RUST SERVER FILTER
           </h2>
           <h3 className="hidden md:block [text-shadow:_1px_1px_0_rgb(0_0_0_/_40%)]">
@@ -26,7 +26,7 @@ function Header() {
           <Link
             key={index} // It's recommended to use a unique key when mapping elements.
             href={link.href}
-            className="text-gray-200 [text-shadow:_3px_3px_0_rgb(0_0_0_/_40%)] font-rust tracking-[0.100rem] text-center px-1 mx-4 rounded sm:w-30 text-lg md:text-lg transition-all cursor-pointer hover:text-white block"
+            className="text-gray-200 m-1 sm:m-0.5 [text-shadow:_2px_2px_2px_rgb(0_0_0_/_60%)] font-rust tracking-[0.100rem] text-center px-1 mx-4 rounded sm:w-30 text-lg md:text-lg transition-all cursor-pointer hover:text-white block"
           >
             {link.text}
           </Link>
