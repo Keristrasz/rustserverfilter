9a4faef2cc6e64a90aebde06cbb109f302684911
Karel Ježík <jezikkarel@gmail.com>
2023-10-13 17:44:19 +0200

    vv

--- components/UI/Layout/subcomponents/AnimatedBackground.tsx
+++ components/UI/Layout/subcomponents/AnimatedBackground.tsx
@@ -1,10 +1,10 @@
 const AnimatedBackground = () => {
   return (
-    <div className="w-full max-h-[100%] absolute z-[-1] overflow-hidden">
+    <div className="w-full max-h-[100%] fixed z-[-1]">
       {/* <video className="w-full max-h-[100%] object-cover" autoPlay loop muted>
         <source src="/sea1080-2.mp4" type="video/mp4" />
       </video> */}
-      <video className="w-full max-h-[100%] object-cover" autoPlay loop muted>
+      <video className="w-full max-h-[100%]" autoPlay loop muted>
         <source src="/rust-dome.mp4" type="video/mp4" />
       </video>
     </div>
