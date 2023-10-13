b79d8b2894a60f1c5f4a22477fd119278396b51e
Karel Ježík <jezikkarel@gmail.com>
2023-10-13 17:48:30 +0200

    s

---
+++ components/layout/AnimatedBackground.tsx
@@ -0,0 +1,14 @@
+const AnimatedBackground = () => {
+  return (
+    <div className="w-full max-h-[100%] absolute z-[-1] overflow-hidden">
+      {/* <video className="w-full max-h-[100%] object-cover" autoPlay loop muted>
+        <source src="/sea1080-2.mp4" type="video/mp4" />
+      </video> */}
+      <video className="w-full max-h-[100%] object-cover" autoPlay loop muted>
+        <source src="/rust-dome.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+
+export default AnimatedBackground;
