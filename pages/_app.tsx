754a35ba509f6427547b424c22ed4863dcac8af4
Karel Ježík <jezikkarel@gmail.com>
2024-01-14 15:55:39 +0100

    return of unsafescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from "next/app";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import Head from "next/head";
+// import { useRouter } from "next/router";
+// import Head from "next/head";
 import Script from "next/script";
 import ErrorBoundary from "../components/HOC/ErrorBoundary";
 import { Layout } from "../components/UI/Layout";
@@ -12,20 +12,23 @@ import { ToastContainer } from "react-toastify";
 import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
-import * as gtag from "../scripts/gtag";
+import { headers } from "next/headers";
+// import * as gtag from "../scripts/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  const nonce = headers().get("x-nonce") || undefined;
+
+  // const router = useRouter();
+  // useEffect(() => {
+  //   const handleRouteChange = (url: any) => {
+  //     gtag.pageview(url);
+  //   };
+  //   router.events.on("routeChangeComplete", handleRouteChange);
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, [router.events]);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -33,29 +36,14 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
-        {/* LAST WORKING VERSION */}
-        {/* <Script
-          id="ga-tracking"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${gtag.GA_TRACKING_ID}', {
-        page_path: window.location.pathname,
-      });
-    `,
-          }}
-        /> */}
-        <Script id="ga-tracking" strategy="afterInteractive">
+        <Script nonce={nonce} id="ga-tracking" strategy="afterInteractive">
           {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '${gtag.GA_TRACKING_ID}', {
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
       page_path: window.location.pathname,
     });
   `}
